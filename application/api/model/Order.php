a12b0d37009eee9a5369b4630a4a1fb395376b0e
skr <1095421013@qq.com>
2019-08-01 17:18:57 +0800

    新增order模型

---
+++ application/api/model/Order.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Author: Luoaotian
+ * Date: 2019/7/31
+ * Time: 11:29
+ */
+
+namespace app\api\model;
+
+
+class Order extends BaseModel
+{
+    protected $autoWriteTimestamp = true;
+}
