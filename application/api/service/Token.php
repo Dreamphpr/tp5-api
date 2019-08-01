eefdc96a9b346a69522f8d3c1bed10c4f6e4ff6f
skr <1095421013@qq.com>
2019-08-01 17:19:39 +0800

    新增Order service

--- application/api/service/Token.php
+++ application/api/service/Token.php
@@ -8,6 +8,8 @@
 namespace app\api\service;
 
 
+use app\lib\enum\ScopeEnum;
+use app\lib\exception\ForbiddenException;
 use app\lib\exception\TokenException;
 use think\Cache;
 use think\Exception;
@@ -15,6 +17,7 @@ use think\Request;
 
 class Token
 {
+    //生成token
     public static function generateToken()
     {
         $randChars = getRandChar(32);
@@ -25,6 +28,7 @@ class Token
 
     }
 
+    //获取token里的值
     public static function getCurrentTokenVar($key)
     {
         $token = Request::instance()->header('token');
@@ -45,9 +49,58 @@ class Token
         }
     }
 
+    //获取token里的uid
     public static function getCurrentUid()
     {
         $uid = self::getCurrentTokenVar('uid');
         return $uid;
+    }
+
+    //用户或管理员访问权限
+    public static function needPrimaryScope()
+    {
+        $scope = self::getCurrentTokenVar('scope');
+        if($scope){
+            if($scope >= ScopeEnum::User){
+                return true;
+            }
+            else{
+                throw new ForbiddenException();
+            }
+        }
+        else{
+            throw new TokenException();
+        }
+    }
+
+    //只有用户访问权限
+    public function needExclusiveScope()
+    {
+        $scope = self::getCurrentTokenVar('scope');
+        if($scope){
+            if($scope == ScopeEnum::User){
+                return true;
+            }
+            else{
+                throw new ForbiddenException();
+            }
+        }
+        else{
+            throw new TokenException();
+        }
+
+    }
+
+    //
+    public static function isValidOperate($checkUID)
+    {
+        if(!$checkUID){
+            throw new Exception('检查UID时必须传入一个被检查的UID');
+        }
+        $uid = self::getCurrentUid();
+        if($uid == $checkUID){
+            return true;
+        }
+        return false;
     }
 }
