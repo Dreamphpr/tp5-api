9384f4a0cf9d861e0f1144feecc3a03f3a6cb359
skr <1095421013@qq.com>
2019-08-02 17:38:54 +0800

    新增路由

--- application/route.php
+++ application/route.php
@@ -11,4 +11,35 @@
 
 use think\Route;
 
-Route::get('banner/:id','api/v1.Banner/getBanner');
+Route::get('api/:version/banner/:id','api/:version.Banner/getBanner');
+
+Route::get('api/:version/theme','api/:version.Theme/getSimpleList');
+
+Route::get('api/:version/theme/:id','api/:version.Theme/getComplexOne');
+
+Route::group('api/:version/product',function (){
+
+    Route::get('/:id','api/:version.Product/getOne',[],['id'=>'\d+']);
+
+    Route::get('/recent','api/:version.Product/getRecent');
+
+    Route::get('/by_category','api/:version.Product/getAllInCategory');
+
+});
+
+
+Route::get('api/:version/category/all','api/:version.Category/getAllCategories');
+
+Route::post('api/:version/token/user','api/:version.Token/getToken');
+
+Route::post('api/:version/address','api/:version.Address/createOrUpdateAddress');
+
+Route::post('api/:version/order','api/:version.Order/placeOrder');
+
+Route::get('api/:version/order/:id','api/:version.Order/getDetail',[],['id'=>'\d+']);
+
+Route::post('api/:version/by_user','api/:version.Order/getSummaryByUser');
+
+Route::post('api/:version/pay/pre_order','api/:version.Pay/getPreOrder');
+
+Route::post('api/:version/pay/notify','api/:version.Pay/receiveNotify');
